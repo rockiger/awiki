7c2f1bfe58a5296d11c230e2c2bf5025ccfe7f6b
Marco Laspe <marco@rockiger.com>
2018-07-01 07:35:43 +0200

    create initial sidebar and open a file on click

--- src/app.js
+++ src/app.js
@@ -8,28 +8,95 @@ import "./helpers/external_links.js";
 // Everything below is just to show you how it works. You can delete all of it.
 // ----------------------------------------------------------------------------
 
-import { remote } from "electron";
+/*********
+ * Setup *
+ *********/
 import jetpack from "fs-jetpack";
-import { greet } from "./hello_world/hello_world";
 import env from "env";
 
-const app = remote.app;
-const appDir = jetpack.cwd(app.getAppPath());
+const basePath = "/home/macco/mega/md/"
+const ext = ".markdown";
 
-// Holy crap! This is browser window with HTML and stuff, but I can read
-// files from disk like it's node.js! Welcome to Electron world :)
-const manifest = appDir.read("package.json", "json");
+let tree = jetpack.inspectTree(basePath, {relativePath: true}).children;
+let sidebar = createSidebar(tree);
 
-const osMap = {
-  win32: "Windows",
-  darwin: "macOS",
-  linux: "Linux"
-};
+const nav = document.querySelector("#nav");
+nav.innerHTML = sidebar;
+let labels = document.querySelectorAll("#nav x-label");
 
 document.querySelector("#app").style.display = "flex";
-document.querySelector("#greet").innerHTML = greet();
-document.querySelector("#os").innerHTML = osMap[process.platform];
-document.querySelector("#author").innerHTML = manifest.author;
-document.querySelector("#env").innerHTML = env.name;
-document.querySelector("#electron-version").innerHTML =
-  process.versions.electron;
+
+for (const label of labels) {
+  label.addEventListener('click', onClickLabel)
+}
+
+/*************
+ * Functions *
+ *************/
+
+
+function createSidebar(tree) {
+  if (tree.length === 0) {
+    return "";
+  }
+
+  const first = tree[0]; 
+  const rest = tree.slice(1);
+  const isTxt = first.name.endsWith(ext);
+  const isDir = (first.type === "dir") ? true : false;
+  const name = isDir ? first.name : first.name.slice(0,-ext.length);
+  if ( isDir && hasFile(name, rest) && hasChild(first)) {
+    return accordion(first) + createSidebar(rest.slice(1)); //
+  } else if (isTxt) {
+    return label(name, first) + createSidebar(rest);
+  } else {
+    return createSidebar(rest);
+  }
+}
+
+function hasFile(name, list) {
+  console.log(name);
+  console.log(list)
+  for (const e of list) {
+    console.log(e.name);
+    if (name + ext === e.name) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function hasChild(el) {
+  for (const child of el.children) {
+    if (child.name.endsWith(ext)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function label(name, el) {
+  return `<x-label data-path="${el.relativePath}">${name}</x-label>`;
+}
+
+function accordion(el, expanded="expanded") {
+  return `<x-accordion ${expanded}>
+  <header>
+    ${label(el.name, el)}
+  </header>
+
+  <main>
+    ${createSidebar(el.children)}
+  </main>
+</x-accordion>`
+}
+
+
+function onClickLabel(ev) {
+  const filePath = ev.target.dataset.path.endsWith(ext) ? ev.target.dataset.path : ev.target.dataset.path + ext;
+  console.log(jetpack.read(basePath + filePath)); // TODO path with 
+  const editor = document.querySelector("#main > x-textarea");
+  editor.value = jetpack.read(basePath + filePath);
+  ev.stopPropagation();
+  return false;
+}
